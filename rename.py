98389c89c3cd283daf4453db29925d75045b9841
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-17 08:45:47 +0530

    necessary comments added

--- rename.py
+++ rename.py
@@ -8,11 +8,13 @@ operation=Operation()
 def main():
     pass
 
+# list files in any directory
 @main.command()
 @click.option('--path',prompt='Type desired path', required=True, type=str)
 def readdir(path):
     operation.read(path)
 
+# list files in choice
 @main.command()
 @click.option('--path', prompt='Type desired path', required=True, type=str)
 def list(path):
