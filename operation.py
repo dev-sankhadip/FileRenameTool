6ba58c0189da85d3abf91ea1d6400728e8641d9a
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-21 18:39:44 +0530

    code: listing files in size added

--- operation.py
+++ operation.py
@@ -11,7 +11,6 @@ class Operation:
             print(f)
     
     def listType(self, path):
-
         # questions to user
         questions = [
             {
@@ -26,29 +25,44 @@ class Operation:
         if os.path.isdir(path):
             answers = prompt(questions, style=custom_style_2)
             fileTypeId =  answers['type'].split('.')
+
             # get all files inside directory
             files = os.listdir(path)
+
+            # add file to array with original base path
+            allFile=[]
+            for f in files:
+                allFile.append(path+'/'+f)
+
 
             # if choice is in alphabetical order
             if fileTypeId[0]=='1':
                 for f in files:
                     print(f)
-            
+
             # if choice is in file creation time
             elif fileTypeId[0]=='2':
-                allFile=[]
-                for f in files:
-                    allFile.append(path+'/'+f)
+                # allFile=[]
+                # for f in files:
+                #     allFile.append(path+'/'+f)
                 allFiles = sorted(allFile, key=os.path.getctime)
                 for file in allFiles:
-                    fileArray = file.split('/')
-                    fileName = fileArray[len(fileArray)-1]
-                    print(fileName)
-            
+                    # fileArray = file.split('/')
+                    # fileName = fileArray[len(fileArray)-1]
+                    # print(fileName)
+                    print(self.getFileName(file))
+
             # if choice is in file size
             elif fileTypeId[0]=='3':
-                print("in file size")
+                allFiles = sorted(allFile, key=os.path.getsize)
+                for file in allFiles:
+                    print(self.getFileName(file))
         
         # if path is not directory
         else:
             print("Invalid directory path")
+
+    def getFileName(self, file):
+        fileArray = file.split('/')
+        fileName = fileArray[len(fileArray)-1]
+        return fileName
